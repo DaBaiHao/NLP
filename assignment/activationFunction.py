420291e8050043d1a4ef3e1339f36eed122b5ff7
Hao Bai <baihaozi12@gmail.com>
2019-01-23 23:34:10 +0000

    make use of tensornoard

--- assignment/activationFunction.py
+++ assignment/activationFunction.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 
 def add_layer(inputs, in_size, out_size, activation_function = None):
     with tf.name_scope('layer'):
-        Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-        biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-        Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        with tf.name_scope('Weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='Weight')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='biases')
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
 
         if activation_function is None:
             outputs = Wx_plus_b
@@ -19,20 +22,27 @@ x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
+# define place holder for input to network
 with tf.name_scope('input'):
     xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
     ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
+
+# add hidden layer
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# add output layer
 predition = add_layer(l1, 10, 1, activation_function=None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - predition),
-                                    reduction_indices=[1]))
-
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+# the error between predicton and real data
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - predition),
+                                        reduction_indices=[1]))
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.initialize_all_variables()
 
 sess = tf.Session()
+writer = tf.summary.FileWriter("logs/", sess.graph)
 sess.run(init)
 
 
